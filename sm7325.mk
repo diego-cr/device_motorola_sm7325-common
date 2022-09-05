c3fd7bd9f1d035b6c9af2aef826ee3c62f7ff006
diegocr@gmail.com <diegocr@gmail.com>
2022-09-05 13:25:49 +0200

    sm7325-common: Aicpify!
    Change-Id: I6efa893bd1fe389f44d4d4c81d46cabe59e4ea97

--- sm7325.mk
+++ sm7325.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aicp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
